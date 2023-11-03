6a68edfd3524e98327af6bb85f55896419e6f2dd
SergTa <88887008+SergTa@users.noreply.github.com>
2023-11-03 20:51:32 +0300

    Update Sent.py

--- Sent.py
+++ Sent.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 classifier = pipeline ("sentiment-analysis,
-                       blanchefort/rubert-base-cased-sentiment"
+                       blanchefort/rubert-base-cased-sentiment")
 classifier("У меня репозиторий не клонируется в виртуальный линукс. 
 Пишет Не удалось прочитать из внешнего репозитория. Удостоверьтесь, 
 что у вас есть необходимые права доступа и репозиторий существует")
